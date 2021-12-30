df42570e1f9dfa34f0a11c9a33edcfb37e5cc733
rolandoguerrero <rolando22_@outlook.com>
2021-12-30 11:25:10 -0500

    deport

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT;
-
+const port = process.env.PORT || 3000;
+require('dotenv').config()
 server.use(middlewares);
 server.use(router);
-server.listen(port);
+server.listen(port,()=>{
+    console.log(`connect  ${port}`)
+});
+
