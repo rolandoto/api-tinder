847d0cfc100605fbec49d5d6550679d0e9476cd5
rolandoguerrero <rolando22_@outlook.com>
2021-12-30 10:46:13 -0500

    cuarto commit

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = 3000
+const port = process.env.PORT;
+
 server.use(middlewares);
 server.use(router);
 server.listen(port);
