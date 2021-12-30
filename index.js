6df10dd911361d9b8640d8255bd824593e68d963
rolandoguerrero <rolando22_@outlook.com>
2021-12-30 10:41:58 -0500

    definit

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-
+const port = 3000
 server.use(middlewares);
 server.use(router);
-
-server.listen(3000,()=>{
-    console.log('connect')
-});
+server.listen(port);
