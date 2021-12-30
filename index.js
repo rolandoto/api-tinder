ef0d46d589a7db0ec6da6b743a1f480445eb84e2
rolandoguerrero <rolando22_@outlook.com>
2021-12-30 10:39:04 -0500

    tercer commit

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(3000,()=>{
+    console.log('connect')
+});
