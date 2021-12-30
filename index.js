8308db0b3d46f6e85778ca4112619b6121b052ca
rolandoguerrero <rolando22_@outlook.com>
2021-12-30 10:36:05 -0500

    primer

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
-server.use(middlewares)
-server.use(router)
-server.listen(3000, () => {
-  console.log('server connect ')
-})
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
